17661335ab12ff4142731cdb7e20aa465c1d2074
Laszlo Sarvold <laszlos@joinbrella.com>
2022-08-30 11:40:41 -0300

    flexible component

--- src/App.js
+++ src/App.js
@@ -1,51 +1,41 @@
-import React, { useState } from "react";
+import React, {useState, useEffect} from 'react';
 import AgoraUIKit from "agora-react-uikit";
+import axios from 'axios';
 
-//For rtcProps you must set the following properties:
-//appId: The App ID of your Agora account.
-//channel: The channel name for the AgoraRTC session.
-//token: The token for authentication.
-//uid: The user ID for this client.
-//For callbacks you must set the following properties:
-//EndCall: The callback function for ending the call.
+const VideoCall = ({id, data, rol}) => {
+    const [videoCall, setVideoCall] = useState(false);
+    const channel = data.queueId;
+    const rtcProps = {
+      appId: process.env.REACT_APP_AGORA_APP_ID,
+      channel: channel,
+      token:"",
+      uid: id,
+    };
 
-//Token get url sintax is
-//Base url https://mediline-agora-token-service.herokuapp.com/rtc/:channel/:role/:uid/
-//:channel is the channel name
-//:role is the role of the user (publisher or subscriber)
-//:uid is the user id
-//Example: https://mediline-agora-token-service.herokuapp.com/rtc/test/publisher/uid/123456/
+    useEffect(() =>{
+      const getToken = async () =>{
+        const url = `${process.env.REACT_APP_AGORA_TOKEN_SERVICE}/rtc/${rtcProps.channel}/${rol}/uid/${rtcProps.uid}`;
+        const response = await axios.get(url)
+        const token = response.data.token;
+        rtcProps.token = token;
+        //eslint-disable-next-line
+        console.log(rtcProps);
+        //eslint-disable-next-line
+        console.log(data);
+      }
 
-const App = () => {
-  const [videoCall, setVideoCall] = useState(false);
-  const rtcProps = {
-    appId: "52a29b8c9c6444d2afd6e7038eaa3e0a",
-    channel: "test",
-    token:
-      "00652a29b8c9c6444d2afd6e7038eaa3e0aIAD+z1CZ6wcbLoJuNg3oHxq7rj2LktG4XjAKG6vfNdSoIwx+f9hh03IJIgBYXiwCDHYHYwQAAQCcMgZjAgCcMgZjAwCcMgZjBACcMgZj",
-    uid: 123456,
-  };
+      getToken();
+    },[]);
 
-  // const getToken = async () =>{
-
-  //   const url = `https://mediline-agora-token-service.herokuapp.com/rtc/${rtcProps.channel}/publisher/${rtcProps.uid}`;
-  //   const response = await axios.get(url)
-  //   const token = response.data.token;
-  //   rtcProps.token = token;
-  //   setVideoCall(true);
-  // }
-
-  const callbacks = {
-    EndCall: () => setVideoCall(false),
-  };
-  return videoCall ? (
-    <div style={{ display: "flex", width: "100vw", height: "100vh" }}>
-      <h1>Mediline</h1>
-      <AgoraUIKit rtcProps={rtcProps} callbacks={callbacks} />
-    </div>
-  ) : (
-    <h3 onClick={() => setVideoCall(true)}>Start Call</h3>
-  );
+    const callbacks = {
+      EndCall: () => setVideoCall(false),
+    };
+    
+   return (
+    <h1>Hola</h1>
+   );
 };
+// <AgoraUIKit rtcProps={rtcProps} callbacks={callbacks} /> 
 
-export default App;
+
+export default VideoCall;
